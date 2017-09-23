56a51d8b543bd0baa0b1233de0724c9ab7b00712
Tim Klingeleers <Mardaneus86@users.noreply.github.com>
2017-09-23 14:14:46 +0200

    feat: instantly go to the transfer search screen

---
+++ instant-transfer-search.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         FUT Instant Transfer Search
+// @namespace    https://www.easports.com/
+// @version      0.1
+// @description  Instantly go to the Transfer Search
+// @author       Tim Klingeleers
+// @match        https://www.easports.com/fifa/ultimate-team/web-app/*
+// @grant        GM_xmlhttpRequest
+// @connect      *
+// ==/UserScript==
+(function() {
+  'use strict';
+  getStorage().setItem("deepLinkURL", enums.DeepLinkSections.AUCTION);
+})();
