50856fdb579b6db232d6c49f504b64694d4bdaf1
Tim Klingeleers <Mardaneus86@users.noreply.github.com>
2017-09-23 14:25:24 +0200

    chore: change meta

--- instant-transfer-search.user.js
+++ instant-transfer-search.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
-// @name         instant-transfer-search
-// @namespace    https://www.easports.com/
-// @version      0.1
-// @description  Instantly go to the Transfer Search
-// @author       Tim Klingeleers
+// @name        FUT Instant Transfer Search
+// @version     0.1
+// @description Instantly go to the Transfer Search
+// @license     MIT
+// @author      Tim Klingeleers
 // @match        https://www.easports.com/fifa/ultimate-team/web-app/*
-// @grant        GM_xmlhttpRequest
-// @connect      *
+// @namespace   https://github.com/Mardaneus86
+// @grant       GM_xmlhttpRequest
+// @updateURL   https://raw.githubusercontent.com/Mardaneus86/GitHub-userscripts/master/instant-transfer-search.user.js
+// @downloadURL https://raw.githubusercontent.com/Mardaneus86/GitHub-userscripts/master/instant-transfer-search.user.js
 // ==/UserScript==
 (function() {
   'use strict';
