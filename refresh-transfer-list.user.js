18f8bc7542b39fe4787d727bc08255cc52a75238
Tim Klingeleers <Mardaneus86@users.noreply.github.com>
2017-09-25 18:22:28 +0200

    fix: attach event handler to bottom refresh button

--- refresh-transfer-list.user.js
+++ refresh-transfer-list.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        FUT Refresh Transfer List
-// @version     0.1.1
+// @version     0.1.2
 // @description Refresh Transfer List
 // @license     MIT
 // @author      Tim Klingeleers
@@ -19,7 +19,7 @@
   $(document).bind('DOMNodeInserted', function (event) {
     if ($(event.target).hasClass("SearchResults")) {
       if ($(event.target).find('.refreshList').length === 0) {
-        $(event.target).find('.pagingContainer').append('<a class="btn-flat pagination next" style="float: right" class="refreshList">Refresh</a>');
+        $(event.target).find('.pagingContainer').append('<a class="btn-flat pagination next refreshList" style="float: right">Refresh</a>');
         $('.refreshList').click(function () {
           gNavManager.getCurrentScreenController()._controller._listController._requestItems();
         });
