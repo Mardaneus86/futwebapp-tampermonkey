a9802859a337dd28f23efe719cea91fa817ed55a
Tim Klingeleers <Mardaneus86@users.noreply.github.com>
2017-10-04 18:48:18 +0200

    fix: reduce load on futbin
    getting player prices for a page in one call, and player prices are only fetched once per page
    
    fixes #14

--- show-futbin-player-price.user.js
+++ show-futbin-player-price.user.js
@@ -17,8 +17,10 @@
 // ==/OpenUserJS==
 (function () {
   'use strict';
-
-  var showFutbinPrice = function showFutbinPrice(target) {
+  var showFutbinPrice = function showFutbinPrice(target, futbinData) {
+    if (!futbinData) {
+      return;
+    }
     if (target.find('.player').length === 0) {
       // not a player
       return;
@@ -35,45 +37,115 @@
     if(playerId.startsWith('p')) {
       playerId = playerId.substr(1);
     }
+    
+    if (!futbinData[playerId]) {
+      return; // futbin data might not be available for this player
+    }
 
-    var futbinUrl = "https://www.futbin.com/18/playerPrices?player=" + playerId;
-
-    GM_xmlhttpRequest({
-      method: "GET",
-      url: futbinUrl,
-      onload: function (res) {
-        var data = JSON.parse(res.response);
-        
-        if (target.find('.futbin').length > 0) {
-          return; // futbin price already added to the row
+    if (target.find('.futbin').length > 0) {
+      return; // futbin price already added to the row
+    }
+    
+    var targetForButton = null;
+    switch(gNavManager.getCurrentScreen()._screenId) {
+      case "TradePile":
+      case "MyClubSearch":
+        $(".secondary.player-stats-data-component").css('float', 'left');
+        targetForButton = target.find('.entityContainer');
+        targetForButton.append('<div class="auction futbin" style="margin: 0; width: auto;"><span class="label">Futbin BIN</span><span class="coins value">' + futbinData[playerId].prices[platform].LCPrice + '</span></div>');
+        break;
+      case "SearchResults":
+        if (!futbinData[playerId]) {
+          debugger;
         }
-        
-        var targetForButton = null;
-        switch(gNavManager.getCurrentScreen()._screenId) {
-          case "TradePile":
-          case "MyClubSearch":
-            $(".secondary.player-stats-data-component").css('float', 'left');
-            targetForButton = target.find('.entityContainer');
-            targetForButton.append('<div class="auction futbin" style="margin: 0; width: auto;"><span class="label">Futbin BIN</span><span class="coins value">' + data[playerId].prices[platform].LCPrice + '</span></div>');
-            break;
-          case "SearchResults":
-            targetForButton = target.find('.auctionValue').parent();
-            targetForButton.prepend('<div class="auctionValue futbin"><span class="label">Futbin BIN</span><span class="coins value">' + data[playerId].prices[platform].LCPrice + '</span></div>');
-            break;
-        }
-      }
-    });
+        targetForButton = target.find('.auctionValue').parent();
+        targetForButton.prepend('<div class="auctionValue futbin"><span class="label">Futbin BIN</span><span class="coins value">' + futbinData[playerId].prices[platform].LCPrice + '</span></div>');
+        break;
+    };
   };
 
-  $(document).bind('DOMNodeInserted', function (event) {
-    if ($(event.target).hasClass('sectioned-item-list')) {
-      $(event.target).find('.listFUTItem').each(function(index, item) {
-        showFutbinPrice($(item));
-      })
-    }
+  var initialised = false;
+  var lastIndex = -1;
+  var index = 0;
+  var lastScreen = "";
+  var intervalRunning = null;
 
-    if ($(event.target).hasClass("listFUTItem")) {
-      showFutbinPrice($(event.target));
-    }
+  gNavManager.onScreenRequest.observe(this, function(obs, event) {
+      switch(event) {
+        case "TradePile":
+        case "MyClubSearch":
+        case "SearchResults":
+          if(initialised) {
+            return;
+          }
+          intervalRunning = setInterval(function() {
+            var controller = gNavManager.getCurrentScreenController()._controller;
+            
+            if (lastScreen === event && 
+              lastIndex === index) {
+              return;
+            }
+
+            lastScreen = event;
+            lastIndex = index;
+            
+            if (!controller ||
+                !controller._listController ||
+                !controller._listController._view) {
+              return; // only run if data is available
+            }
+
+            var listrows = null;
+            if (controller._listController._view._list &&
+                controller._listController._view._list._listRows &&
+                controller._listController._view._list._listRows.length > 0) {
+              listrows = controller._listController._view._list._listRows; // for transfer market and club search
+            } else if (controller._listController._view._listRows &&
+                controller._listController._view._listRows.length > 0) {
+              listrows = controller._listController._view._listRows; // for trade pile
+            }
+
+            if (listrows === null) {
+              return;
+            }
+
+            // do we have navigation?
+            if (controller._listController._view.addTarget) {
+              // TODO: can we get the index in a better way? right now the viewmodel page index stays 0 while paging occurs
+              controller._listController._view.addTarget(this, function() { index++ }, enums.UIPaginationEvent.NEXT);
+              controller._listController._view.addTarget(this, function() { index-- }, enums.UIPaginationEvent.PREVIOUS);
+            }
+
+            var uiItems = gNavManager.getCurrentScreen().$_root.find('.listFUTItem');
+            
+            var futbinUrl = "https://www.futbin.com/18/playerPrices?player=&all_versions=" + listrows.
+              map(function(i) { return i.data._metaData.id}).
+              filter(function(current, next) { return current !== next}).
+              join(',')
+            GM_xmlhttpRequest({
+              method: "GET",
+              url: futbinUrl,
+              onload: function (res) {
+                var futbinData = JSON.parse(res.response);
+                uiItems.each(function(index, item) {
+                  showFutbinPrice($(item), futbinData);
+                })
+              }
+            });
+            initialised = true;
+          }, 1000);
+          break;
+
+        default:
+          // no need to search prices on other pages
+          // reset page
+          initialised = false;
+          lastIndex = -1;
+          if (intervalRunning) {
+            clearInterval(intervalRunning);
+          }
+          intervalRunning = null;
+          break;
+      }
   });
 })();
