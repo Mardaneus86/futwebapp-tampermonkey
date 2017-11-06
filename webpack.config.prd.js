c0ba1cb96249d96fe1f41d01aabda9a5e0e49b55
Tim Klingeleers <Mardaneus86@users.noreply.github.com>
2017-11-06 10:17:56 +0100

    chore: update Tampermonkey version in header automatically
    follow semantic-release versioning. Currently extracting version number from dry-run output, because semantic-release does not output a version number anywhere else.

--- webpack.config.prd.js
+++ webpack.config.prd.js
@@ -3,18 +3,20 @@ var fs = require("fs");
 const webpack = require('webpack');
 const config = require('./webpack.config');
 
-let header = fs.readFileSync('./tampermonkey-headers.js', 'utf8');
-header = header.replace('VERSION', require("./package.json").version);
+module.exports = env => {
+  let header = fs.readFileSync('./tampermonkey-headers.js', 'utf8');
+  header = header.replace('VERSION', process.env.TM_VERSION); // set by the build process on Travis
+  
+  console.log('Changed Tampermonkey header version to ' + process.env.TM_VERSION);
+  
+  config.devtool = 'none';
+  config.plugins = [
+    new webpack.BannerPlugin({ 
+      banner: header,
+      raw: true, 
+      entryOnly: true 
+    }),
+  ];
 
-console.log('Changed Tampermonkey header version to ' + require("./package.json").version);
-
-config.devtool = 'none';
-config.plugins = [
-  new webpack.BannerPlugin({ 
-    banner: header,
-    raw: true, 
-    entryOnly: true 
-  }),
-];
-
-module.exports = config;
+  return config;
+};
