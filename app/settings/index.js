c8e37d9e0db4ef59ea0f53b283b31716d60ce626
Tim Klingeleers <Mardaneus86@users.noreply.github.com>
2017-11-07 20:21:19 +0100

    fix: store subsetting values
    subsetting values were not saved, so the defaults were always used

--- app/settings/index.js
+++ app/settings/index.js
@@ -4,6 +4,21 @@
 import './index.scss';
 import settingsPage from './html/index/settings.html';
 
+const handleFieldChange = (entry, setting, e) => {
+  if (setting.subsettings && setting.subsettings.length > 0) {
+    entry.changeValue(setting.key, e.target.checked);
+  } else {
+    entry.changeValue(setting.key, e.target.value);
+  }
+
+  if (setting.callback) {
+    setting.callback(e.target.value);
+  }
+  if (setting.subsettings && setting.subsettings.length > 0) {
+    $(`[data-parent-feature-setting-id='${entry.id}:${setting.key}']`).toggle();
+  }
+};
+
 export default (settings) => {
   const html = settingsPage;
 
@@ -18,38 +33,32 @@ export default (settings) => {
       for (const setting of entry.settings) {
         if (setting.subsettings.length > 0) {
           const subChecked = setting.value ? 'checked="checked"' : '';
-          settingsFields += `<input type="checkbox" id="${`${entry.id}:${setting.key}`}" data-feature-setting-id="${`${entry.id}:${setting.key}`}" ${subChecked}></input>
-            <label for="${`${entry.id}:${setting.key}`}">${setting.label}</label>`;
+          settingsFields += `<input type="checkbox" id="${entry.id}:${setting.key}" data-feature-setting-id="${entry.id}:${setting.key}" ${subChecked}></input>
+            <label for="${entry.id}:${setting.key}">${setting.label}</label>`;
 
           const settingActive = setting.value ? 'block' : 'none';
-          settingsFields += `<div data-parent-feature-setting-id="${`${entry.id}:${setting.key}`}" style="display: ${settingActive}">`;
+          settingsFields += `<div data-parent-feature-setting-id="${entry.id}:${setting.key}" style="display: ${settingActive}">`;
           for (const subsetting of setting.subsettings) {
-            settingsFields += `<div class="setting"><label for="${`${entry.id}:${subsetting.key}`}">${subsetting.label}</label>
-              <input type="text" id="${`${entry.id}:${subsetting.key}`}" data-feature-setting-id="${`${entry.id}:${subsetting.key}`}" value="${subsetting.value}"></input></div>`;
+            settingsFields += `<div class="setting"><label for="${entry.id}:${subsetting.key}">${subsetting.label}</label>
+              <input type="text" id="${entry.id}:${subsetting.key}" data-feature-setting-id="${entry.id}:${subsetting.key}" value="${subsetting.value}"></input></div>`;
           }
           settingsFields += '</div>';
         } else {
-          settingsFields += `<div class="setting"><label for="${`${entry.id}:${setting.key}`}">${setting.label}</label>
-            <input type="text" id="${`${entry.id}:${setting.key}`}" data-feature-setting-id="${`${entry.id}:${setting.key}`}" value="${setting.value}"></input></div>`;
+          settingsFields += `<div class="setting"><label for="${entry.id}:${setting.key}">${setting.label}</label>
+            <input type="text" id="${entry.id}:${setting.key}" data-feature-setting-id="${entry.id}:${setting.key}" value="${setting.value}"></input></div>`;
         }
       }
       const featureActive = entry.isActive ? 'block' : 'none';
       settingsPanel.append(`<div class="feature-settings"><div data-feature-settings="${entry.id}" style="display: ${featureActive};">${settingsFields}</div></div>`);
       for (const setting of entry.settings) {
-        $(`[data-feature-setting-id='${`${entry.id}:${setting.key}`}']`).on('change', (e) => {
-          if (setting.subsettings.length > 0) {
-            entry.changeValue(setting.key, e.target.checked);
-          } else {
-            entry.changeValue(setting.key, e.target.value);
-          }
-
-          if (setting.callback) {
-            setting.callback(e.target.value);
-          }
-          if (setting.subsettings.length > 0) {
-            $(`[data-parent-feature-setting-id='${`${entry.id}:${setting.key}`}']`).toggle();
-          }
+        $(`[data-feature-setting-id='${entry.id}:${setting.key}']`).on('change', (e) => {
+          handleFieldChange(entry, setting, e);
         });
+        for (const subsetting of setting.subsettings) {
+          $(`[data-feature-setting-id='${entry.id}:${subsetting.key}']`).on('change', (e) => {
+            handleFieldChange(entry, subsetting, e);
+          });
+        }
       }
     } else {
       settingsPanel.append('<div class="feature-settings-empty"></div>');
