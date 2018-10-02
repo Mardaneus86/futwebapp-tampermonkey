bf0153f6f96188e7f9bfa10e73458878dd3daa54
adam j. sontag <ajpiano@ajpiano.com>
2018-10-02 00:19:51 -0400

    Add 'Instant BIN confirm' as a Setting (related to #111)

---
+++ app/instant-bin-confirm/index.js
@@ -0,0 +1,8 @@
+import { InstantBinConfirmSettings } from './settings-entry';
+import { InstantBinConfirm } from './instant-bin-confirm';
+
+export {
+  InstantBinConfirmSettings,
+};
+
+new InstantBinConfirm(); // eslint-disable-line no-new
