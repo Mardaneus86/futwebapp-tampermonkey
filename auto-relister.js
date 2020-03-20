23e41f8c0eb00800ffc4a2da0d8b38313d1396ac
Stuart Nightingale <snightingale@updraftplus.com>
2020-03-20 18:17:52 +0000

    updated name

--- auto-relister.js
+++ auto-relister.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         auto-relister
+// @name         FUT Auto Re-Lister
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  automatically relist all transfers
